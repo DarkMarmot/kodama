64c438f14679e883d15c35dcac66fc122ad5fcd3
Scott Southworth <DarkMarmot@users.noreply.github.com>
2015-06-02 17:02:59 -0500

    Update kodama.js

--- kodama.js
+++ kodama.js
@@ -12,7 +12,7 @@
  * ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  *
- * @author Scott Southworth @DarkMarmot
+ * @authors Scott Southworth @DarkMarmot
  *
  */
 
@@ -385,17 +385,20 @@
 
     };
 
+    var selector = typeof jQuery !== 'undefined' && jQuery !== null ? jQuery : null;
+    selector = selector || (typeof Zepto !== 'undefined' && Zepto !== null ? Zepto : null);
+    if(selector) {
+        selector.fn.kodama = $.fn.kodama_tooltip = $.fn.bamboo = $.fn.kodama || function(tooltipData, tooltipOptions){
+
+            var self = this;
+            var els = self.toArray();
+            var arr = d3.range(els.length).map(function(){return tooltipData;});
+            d3.selectAll(els).data(arr).call(d3.kodama.tooltip().options(tooltipOptions));
+
+            return this;
+
+        };
+    }
     return kodama;
 
 }));
-
-$.fn.kodama = $.fn.kodama_tooltip = $.fn.bamboo = $.fn.kodama || function(tooltipData, tooltipOptions){
-
-    var self = this;
-    var els = self.toArray();
-    var arr = d3.range(els.length).map(function(){return tooltipData;});
-    d3.selectAll(els).data(arr).call(d3.kodama.tooltip().options(tooltipOptions));
-
-    return this;
-
-};
