2255dd7d61643d419f6a0afe7dba18e2b9097acc
Scott Southworth <DarkMarmot@users.noreply.github.com>
2015-06-03 09:40:07 -0500

    Update kodama.js

--- js/kodama.js
+++ js/kodama.js
@@ -12,7 +12,7 @@
  * ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  *
- * @authors Scott Southworth @DarkMarmot
+ * @authors Scott Southworth @DarkMarmot scott.southworth@gmail.com
  *
  */
 
